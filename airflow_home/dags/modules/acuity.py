0ae2a53b27f1ae67f64e6a36cf8f1be99d0bcd12
Andrea Egan <aegan526@gmail.com>
2020-04-15 12:35:51 -0700

    Add dag for citytest sf API

--- airflow_home/dags/modules/acuity.py
+++ airflow_home/dags/modules/acuity.py
@@ -1,41 +1,38 @@
+"""Functions related to interacting with Acuity Scheduling."""
 import os
-import sentry_sdk
 
 from datetime import date
+from urllib.parse import urljoin
 import requests
 from requests.auth import HTTPBasicAuth
-from urllib.parse import urljoin
 
 from modules.core import Core
 
 class Acuity(Core):
-
-    def __init__(self):
-        super().__init__()
-        self.base_url = os.environ.get('ACUITY_BASE_URL')
-        self.appt_type = os.environ.get('ACUITY_APPT_TYPE')
-        self.user = os.environ.get('ACUITY_USER')
-        self.pwd = os.environ.get('ACUITY_PASSWORD')
-
+    """Functions related to interacting with Acuity Scheduling."""
+    @staticmethod
+    #pylint: disable=too-many-arguments
     def get_appointments_by_type(
-            self,
-            base_url = os.environ.get('ACUITY_BASE_URL'),
-            appt_type = os.environ.get('ACUITY_APPT_TYPE'),
-            user = os.environ.get('ACUITY_USER'),
-            pwd = os.environ.get('ACUITY_PASSWORD'),
+            base_url=os.environ.get('ACUITY_BASE_URL'),
+            appt_type=os.environ.get('ACUITY_APPT_TYPE'),
+            user=os.environ.get('ACUITY_USER'),
+            pwd=os.environ.get('ACUITY_PASSWORD'),
             start_date=date.today(),
             end_date=date.today(),
             max_records=1000
         ):
+        """Get Acuity appts based on the appt type and the option to filter by day."""
         url = urljoin(base_url, '/api/v1/appointments')
-        params={
+        params = {
             'appointmentTypeID': appt_type,
             'canceled': False,
             'max': max_records
         }
 
-        if start_date: params['minDate'] = start_date.strftime('%Y-%m-%d')
-        if end_date: params['maxDate'] = end_date.strftime('%Y-%m-%d')
+        if start_date:
+            params['minDate'] = start_date.strftime('%Y-%m-%d')
+        if end_date:
+            params['maxDate'] = end_date.strftime('%Y-%m-%d')
 
 
         response = requests.get(
@@ -48,8 +45,14 @@ class Acuity(Core):
 
         return response.json()
 
-
-    def get_appointment(self, appt_id, base_url,user,pwd):
+    @staticmethod
+    def get_appointment(
+            appt_id,
+            base_url=os.environ.get('ACUITY_BASE_URL'),
+            user=os.environ.get('ACUITY_USER'),
+            pwd=os.environ.get('ACUITY_PASSWORD')
+        ):
+        """Get a single Acuity appointment given an appointment Id. """
         url = urljoin(base_url, '/api/v1/appointments/{}'.format(appt_id))
 
         response = requests.get(
--- airflow_home/dags/modules/citytest_sf_appointments.py
+++ airflow_home/dags/modules/citytest_sf_appointments.py
@@ -1,9 +1,15 @@
+"""Functions related to business logic around CityTestSF appts."""
 import collections
+
+import sentry_sdk
 
 from modules.core import Core
 
 class CityTestSFAppointments(Core):
-    def parse_appointment(self, appointment):
+    """Functions related to business logic around CityTestSF appts."""
+    @staticmethod
+    def parse_appointment(appointment):
+        """Retrieve fields needed from acuity appt."""
         dsw_field_id = 7514073
         form_id = 1368026
         driving_field_id = 7543629
@@ -20,49 +26,72 @@ class CityTestSFAppointments(Core):
         parsed = dict((k, appointment[k]) for k in top_level_fields if k in appointment)
 
         # Get DSW and driving status from form
-        # Looks like next is the most performant way to do this search https://stackoverflow.com/questions/8653516/python-list-of-dictionaries-search
-        form = next((item.get('values') for item in appointment['forms'] if item['id'] == form_id), [])
+        # Looks like next is the most performant way to do this search
+        # https://stackoverflow.com/questions/8653516/python-list-of-dictionaries-search
+        form = next(
+            (item.get('values') for item in appointment['forms'] if item['id'] == form_id),
+            []
+        )
         dsw = next((item.get('value') for item in form if item['fieldID'] == dsw_field_id), None)
         parsed['dsw'] = dsw if dsw else None
-        parsed['applicantWillDrive'] = next((item.get('value') for item in form if item['fieldID'] == driving_field_id), 'Unknown')
+        parsed['applicantWillDrive'] = next(
+            (item.get('value') for item in form if item['fieldID'] == driving_field_id),
+            None
+        )
 
         # Rename id and datetime
         parsed['acuityId'] = parsed.pop('id')
         parsed['appointmentDatetime'] = parsed.pop('datetime')
         parsed['acuityCreatedTime'] = parsed.pop('datetimeCreated')
 
-
         return parsed
 
-
-    def parse_formio_response(self, response):
+    @staticmethod
+    def parse_formio_response(response):
+        """Get desired fields from formio, return dsw and values."""
         data = response['data']
         dsw = data['dsw']
 
         parsed = {
-            'hasNoPCP': data.get('hasPCP', None),
+            'formioId': response['_id'],
             'formioSubmittedTime': response['created'],
+            'hasNoPCP': data.get('hasPCP', None),
             'lastReportedWorkDate': data.get('lastReportedWorkDate', None),
-            'insuranceCarrier': data.get('insuranceCarrier', 'missing'),
+            'insuranceCarrier': data.get('insuranceCarrier', None),
             'kaiserMedicalRecordNumber': data.get('kaiserMedicalRecordNumber', None),
-            'pcpFieldSetIagreetosharemyinformationwithKaiser': data.get('pcpFieldSetIagreetosharemyinformationwithKaiser', None),
-            'formioId': response['_id']
+            'pcpFieldSetIagreetosharemyinformationwithKaiser': data.get(
+                'pcpFieldSetIagreetosharemyinformationwithKaiser', None
+            )
         }
-        if ('pcp' in data): parsed.update(data['pcp'])
+        if 'pcp' in data:
+            parsed.update(data['pcp'])
 
         return (dsw, parsed)
 
-    def check_for_appt_duplicates(self, parsed_appts):
+    @staticmethod
+    def check_for_appt_duplicates(parsed_appts):
+        """Given a list of appointments, check for DSWs that have scheduled multiple appts."""
         dsws = [appt['dsw'] for appt in parsed_appts if appt['dsw']]
-        dup_dsws = { dsw:count for dsw, count in collections.Counter(dsws).items() if count >1 }
-        print('Duplicate DSWs: ', dup_dsws)
+        dup_dsws = {dsw:count for dsw, count in collections.Counter(dsws).items() if count > 1}
+        sentry_sdk.capture_message(
+            'citytest_sf_appointments.check_for_appt_duplicates found duplicate DSWs: {}'.format(
+                dup_dsws
+            ), 'info'
+        )
         return dup_dsws
 
-    def merge_acuity_formio(self, parsed_appointments, parsed_responses):
+    @staticmethod
+    def merge_acuity_formio(parsed_appointments, parsed_responses):
+        """Merge a list of acuity appointments and a dictionary of formio appts keyed on DSW."""
         merged = []
         for appointment in parsed_appointments:
             dsw = appointment['dsw']
             formio_merge = parsed_responses.get(dsw, {}) if dsw else {}
-            if not formio_merge: print('Missing formio data for DSW:', dsw)
+            if not formio_merge:
+                sentry_sdk.capture_message(
+                    'citytest_sf_appointments.merge_acuity_formio missing form.io for dsw {}'.
+                    format(dsw),
+                    'warning'
+                )
             merged.append({**appointment, **formio_merge})
         return merged
